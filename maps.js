08cb97116f190535e4baacff85a210ae62f58708
cm-amos <amosmwongelah@gmail.com>
2021-10-17 20:23:11 +0300

    Update Map.js

---
+++ maps.js
@@ -0,0 +1,11 @@
+let cm = {ename: 'Amos'},
+    brayo = {ename: 'Brian'},
+    jona = {ename: 'Jonathan'}
+
+let empJobs = new Map([
+    [cm, 'Software Engineer'],
+    [brayo, 'Data Analyst'],
+    [jona, 'Network Engineer']
+]);
+
+console.log(empJobs);
