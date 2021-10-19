6395ef96bf1598958d46aae4942e466a0e212df5
cm-amos <amosmwongelah@gmail.com>
2021-10-19 10:27:36 +0300

    Update set in map

---
+++ mapSets.js
@@ -0,0 +1,12 @@
+let andy = {fname: 'Reagan'},
+    jully = {fname: 'Julius'},
+    puri = {fname: 'Purity'}
+
+let emJobs = new Map();
+emJobs.set(andy,'Programmer')
+emJobs.set(jully,'Bloggger')
+emJobs.set(puri,'Youtuber')
+
+console.log(emJobs);
+console.log('Map size is: ' +emJobs.size);
+
