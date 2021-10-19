e2496cb0ef096f8f11d1bdf72d96f3b76e2a7d7d
cm-amos <amosmwongelah@gmail.com>
2021-10-19 10:26:11 +0300

    Update get in map

---
+++ mapGets.js
@@ -0,0 +1,8 @@
+let andy ={ename:"Andrel"},
+    varun = {ename:"Varun"},
+    prijin={ename:"Prijin"}
+let empJobs = new Map([
+    [andy,'Software Architect'],
+    [varun,'Developer']]
+);
+console.log(empJobs.has(andy))
