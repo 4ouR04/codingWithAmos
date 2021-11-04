364e9fc05c92d6908567224efb5da5ec380c4a64
cm-amos <amosmwongelah@gmail.com>
2021-11-04 23:29:24 +0300

    Upload setters.js

--- setters.js
+++ setters.js
@@ -1,19 +1,42 @@
-class Student{
-    contstrucor(roomNo,firstName,lastName){
-        this.rno = roomNo;
-        this.fname = firstName;
-        this.lname = lastName;
+// class Student{
+//     contstrucor(roomNo,firstName,lastName){
+//         this.rno = roomNo;
+//         this.fname = firstName;
+//         this.lname = lastName;
 
-        console.log('Inside construcor');
-    }
-    set rollNo(newRollNo){
-        console.log('Inside setter');
-        this.rno = newRollNo;
-    }
-}
+//         console.log('Inside construcor');
+//     }
+//     set rollNo(newRollNo){
+//         console.log('Inside setter');
+//         this.rno = newRollNo;
+//     }
+// }
 
-let s1 = new Student(101,'Brian','Kituku');
-console.log(s1);
-//we are caling setter
-s1.rollNo = 20;
-console.log(s1)
+// let s1 = new Student(101,'Brian','Kituku');
+// console.log(s1);
+// //we are caling setter
+// s1.rollNo = 20;
+// console.log(s1)
+
+// Create an object:
+const person = {
+  firstName: "Amos",
+  lastName: "Mwongela",
+  language: "en",
+  // get lang() {
+  //   return this.language.toUpperCase();
+  // }
+  // get fullName(){
+  //   return this.firstName + ' ' + this.lastName
+  // }
+
+  // fullName: function(){
+  //   return this.firstName + ' ' + this.lastName
+  //   }
+  fullName(){
+    return this.firstName + " " + this.lastName
+  }
+};
+// console.log(person.fullName());
+
+let demo = document.querySelector(demo).innerHTML = person.fullName();
